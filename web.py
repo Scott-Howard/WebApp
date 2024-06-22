4c4a6746bfe6128211a6b96c7be4ceb85dfafd54
Scott Howard <scottmelvillehoward@gmail.com>
2024-06-22 12:50:55 +1000

    updated to match most recent todo app changes

--- web.py
+++ web.py
@@ -3,13 +3,24 @@ import functions as fn
 
 todos = fn.get_todos()
 
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    fn.write_todos(todos)
+    st.session_state["new_todo"] = ""
+
 st.title("My Todo app")
-st.subheader("this is a subheader")
+st.subheader("this is my todo app")
 st.write("This is to improve your productivity")
 
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        fn.write_todos(todos)
+        del st.session_state[todo]
+        st.session_state["new_todo"] = ""
+        st.experimental_rerun()
 
-for todo in todos:
-    st.checkbox(todo)
-
-
-st.text_input(label="",placeholder="enter a new todo....")
+st.text_input(label="",placeholder="enter a new todo....",
+             on_change=add_todo, key ='new_todo', label_visibility="hidden")
